a3ee6d931544a1d3a39542bb305734997b338364
Guevel Alexis <21701776t>
2019-03-14 10:50:59 +0100

    task

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ let tasks = new CompoTaskList({
     element: document.getElementById('tasks'),
     taskList: list
 });
+let premiereTask = new Task('Première tâche', null);
+let deuxièmeTask = new Task('Deuxième tâche', null);
+let troisièmeTask = new Task('Troisième tâche', null);
+list.add(premiereTask);
+list.add(deuxièmeTask);
+list.add(troisièmeTask);
+tasks.render();
